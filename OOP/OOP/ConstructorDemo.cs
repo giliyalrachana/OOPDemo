6a80785d1040247b8565910ee8946af1ea3d4576
Rachana <giliyalrachana@gmail.com>
2021-09-26 21:25:18 +0530

    New classes are added

---
+++ OOP/OOP/ConstructorDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP
+{
+    // This program demo. how to copy constructor
+    class ConstructorDemo
+    {
+        static void Main()
+        {
+            Student student1 = new Student(456, "Rachana");
+            Student student2 = new Student(student1);
+            Console.WriteLine($"RollNo={student2.StdRollNo}\nName={student2.StdName}");
+            Console.ReadLine();
+
+        }
+    }
+}
